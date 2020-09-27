58d05a8d8b21041ad91c75a2034b4ee023d362c3
Trevor Burn <digitrev@gmail.com>
2020-09-27 09:54:33 -0400

    Pre-2.0

---
+++ Dragalia/DragaliaDB/dbo/Stored Procedures/spGetWeaponRecipe.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spGetWeaponRecipe]
+AS
+SELECT wt.WeaponType
+	,ws.WeaponSource
+	,w.WeaponName
+	,i.ItemName
+	,wc.Quantity
+FROM Weapon AS w
+CROSS APPLY dbo.fnGetWeaponCraftingCounts(w.WeaponID) AS wc
+INNER JOIN WeaponType AS wt ON wt.WeaponTypeID = w.WeaponTypeID
+INNER JOIN WeaponSource AS ws ON ws.WeaponSourceID = w.WeaponSourceID
+INNER JOIN Item AS i ON i.ItemID = wc.ItemID
+ORDER BY w.WeaponID
+	,i.ItemID
