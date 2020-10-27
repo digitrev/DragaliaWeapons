d2b6a8ce60e18343c93cb2020bb0fba2ad1264ab
Trevor Burn <digitrev@gmail.com>
2020-10-27 17:55:28 -0400

    Add util functions

---
+++ Dragalia/DragaliaDB/util/Functions/InlineMax.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [util].[InlineMax] (
+	@m INT
+	,@n INT
+	)
+RETURNS INT
+AS
+BEGIN
+	RETURN (
+			SELECT MAX(v.Val)
+			FROM (
+				VALUES (@m)
+					,(@n)
+				) AS v(val)
+			)
+END
