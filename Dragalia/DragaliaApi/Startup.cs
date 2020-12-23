84b383f54cf2c9dd1e48c5e92af59bb9b58fa9db
Trevor Burn <digitrev@gmail.com>
2020-12-22 22:24:37 -0500

    Re-scaffolded

--- Dragalia/DragaliaApi/Startup.cs
+++ Dragalia/DragaliaApi/Startup.cs
@@ -2,15 +2,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DragaliaApi.Models;
+using DragaliaApi.Data;
 
 namespace DragaliaApi
 {
@@ -27,7 +31,10 @@ namespace DragaliaApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddDbContext<DragaliaContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DragaliaDB")));
+            services.AddControllers().AddNewtonsoftJson(options => 
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DragaliaApi", Version = "v1" });
@@ -37,12 +44,12 @@ namespace DragaliaApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DragaliaApi v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DragaliaApi v1"));
+            //}
 
             app.UseHttpsRedirection();
 
