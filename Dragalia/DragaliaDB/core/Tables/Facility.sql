4f74b501e2478919af121a1ee9fa6afecd11de6f
Trevor Burn <digitrev@gmail.com>
2021-02-18 20:54:26 -0500

    Make category nullable

--- Dragalia/DragaliaDB/core/Tables/Facility.sql
+++ Dragalia/DragaliaDB/core/Tables/Facility.sql
@@ -2,7 +2,7 @@
 	[FacilityID] INT NOT NULL
 	,[Facility] NVARCHAR(50) NOT NULL
 	,[Limit] INT NOT NULL
-	,[CategoryID] INT NOT NULL
+	,[CategoryID] INT NULL
 	,[Active] BIT NOT NULL CONSTRAINT [DF_Facility_Active] DEFAULT(1)
 	,CONSTRAINT [PK_Facility] PRIMARY KEY ([FacilityID])
 	,CONSTRAINT [FK_Facility_Category] FOREIGN KEY ([CategoryID]) REFERENCES [core].[Category]([CategoryID])
--- Dragalia/DragaliaDB/core/Tables/Material.sql
+++ Dragalia/DragaliaDB/core/Tables/Material.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [core].[Material] (
 	[MaterialID] NVARCHAR(50) NOT NULL
 	,[Material] NVARCHAR(50) NOT NULL
-	,[CategoryID] INT NOT NULL
+	,[CategoryID] INT NULL
 	,[Active] BIT NOT NULL CONSTRAINT [DF_Material_Active] DEFAULT(1)
 	,CONSTRAINT [PK_Material] PRIMARY KEY ([MaterialID])
 	,CONSTRAINT [FK_Material_Category] FOREIGN KEY ([CategoryID]) REFERENCES [core].[Category]([CategoryID])
