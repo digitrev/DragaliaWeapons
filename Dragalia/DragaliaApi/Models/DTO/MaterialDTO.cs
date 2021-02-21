e321855fe117a205490e6960c954e3ca10003b65
Trevor Burn <digitrev@gmail.com>
2021-02-21 12:42:06 -0500

    Add hierarchy id to material

---
+++ Dragalia/DragaliaApi/Models/DTO/MaterialDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragaliaApi.Models.DTO
+{
+    public class MaterialDTO
+    {
+        public string MaterialId { get; set; }
+        public string Material1 { get; set; }
+        public int? CategoryId { get; set; }
+        public bool? Active { get; set; }
+        public HierarchyId SortPath { get; set; }
+    }
+}
