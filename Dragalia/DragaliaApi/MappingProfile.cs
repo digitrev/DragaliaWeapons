0d3aea123e87659b91337b1ee12f5cc54f99d620
Trevor Burn <digitrev@gmail.com>
2021-02-25 17:30:26 -0500

    Add some weapon attribute endpoints

--- Dragalia/DragaliaApi/MappingProfile.cs
+++ Dragalia/DragaliaApi/MappingProfile.cs
@@ -15,30 +15,36 @@ namespace DragaliaApi
             CreateMap<Account, AccountDTO>();
             CreateMap<AccountDTO, Account>();
 
-            CreateMap<Weapon, WeaponDTO>()
-                .ForMember(
-                    dest => dest.Element,
-                    opt => opt.MapFrom(src => src.Element.Element1))
-                .ForMember(
-                    dest => dest.WeaponSeries,
-                    opt => opt.MapFrom(src => src.WeaponSeries.WeaponSeries1))
-                .ForMember(
-                    dest => dest.WeaponType,
-                    opt => opt.MapFrom(src => src.WeaponType.WeaponType1))
-                .ForMember(
-                    dest => dest.Weapon,
-                    opt => opt.MapFrom(src => src.Weapon1));
-
             CreateMap<AccountWeapon, AccountWeaponDTO>();
             CreateMap<AccountWeaponDTO, AccountWeapon>();
 
+            CreateMap<Weapon, WeaponDTO>()
+                .ForMember(dest => dest.Element,
+                           opt => opt.MapFrom(src => src.Element.Element1))
+                .ForMember(dest => dest.WeaponSeries,
+                           opt => opt.MapFrom(src => src.WeaponSeries.WeaponSeries1))
+                .ForMember(dest => dest.WeaponType,
+                           opt => opt.MapFrom(src => src.WeaponType.WeaponType1))
+                .ForMember(dest => dest.Weapon,
+                           opt => opt.MapFrom(src => src.Weapon1));
+
+            CreateMap<Element, ElementDTO>()
+                .ForMember(dest => dest.Element,
+                           opt => opt.MapFrom(src => src.Element1));
+
+            CreateMap<WeaponType, WeaponTypeDTO>()
+                .ForMember(dest => dest.WeaponType,
+                           opt => opt.MapFrom(src => src.WeaponType1));
+
+            CreateMap<WeaponSeries, WeaponSeriesDTO>()
+                .ForMember(dest => dest.WeaponSeries,
+                           opt => opt.MapFrom(src => src.WeaponSeries1));
+
             CreateMap<Material, MaterialDTO>()
-                .ForMember(
-                    dest => dest.Category,
-                    opt => opt.MapFrom(src => src.Category.Category1))
-                .ForMember(
-                    dest => dest.Material,
-                    opt => opt.MapFrom(src => src.Material1));
+                .ForMember(dest => dest.Category,
+                           opt => opt.MapFrom(src => src.Category.Category1))
+                .ForMember(dest => dest.Material,
+                           opt => opt.MapFrom(src => src.Material1));
             CreateMap<MaterialDTO, Material>();
         }
     }
