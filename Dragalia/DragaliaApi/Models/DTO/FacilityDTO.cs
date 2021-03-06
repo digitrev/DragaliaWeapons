415c4521e2e51982e4b0eaf58df2342279b27cfc
Trevor Burn <digitrev@gmail.com>
2021-03-06 13:09:26 -0500

    Add facility DTO

---
+++ Dragalia/DragaliaApi/Models/DTO/FacilityDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragaliaApi.Models.DTO
+{
+    public class FacilityDTO
+    {
+        public int FacilityId { get; set; }
+        public string Facility { get; set; }
+        public int Limit { get; set; }
+        public string Category { get; set; }
+    }
+}
