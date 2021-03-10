f6ecd9d417abd3f1fa5665286f01596fc31acc41
Trevor Burn <digitrev@gmail.com>
2021-03-09 19:50:41 -0500

    Add a body filter to inventory

--- Dragalia/DragaliaApi/Startup.cs
+++ Dragalia/DragaliaApi/Startup.cs
@@ -49,12 +49,12 @@ namespace DragaliaApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DragaliaApi v1"));
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DragaliaApi v1"));
+            }
 
             app.UseHttpsRedirection();
 
