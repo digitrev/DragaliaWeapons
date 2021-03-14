312cf9da3530d07ecdbfadd3e99605261a80d2ac
Trevor Burn <digitrev@gmail.com>
2021-03-13 19:35:32 -0500

    Add weapon limit controller

---
+++ Dragalia/DragaliaApi/Controllers/WeaponLimitsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DragaliaApi.Data;
+using DragaliaApi.Models;
+
+namespace DragaliaApi.Controllers
+{
+    [Route("api/WeaponLimits")]
+    [ApiController]
+    public class WeaponLimitsController : ControllerBase
+    {
+        private readonly DragaliaContext _context;
+
+        public WeaponLimitsController(DragaliaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WeaponLimits/unbind
+        [HttpGet("unbind")]
+        public async Task<ActionResult<IEnumerable<WeaponUnbindLimit>>> GetWeaponUnbindLimits(int? rarity)
+        {
+            try
+            {
+                return await _context.WeaponUnbindLimits.Where(wul => rarity == null || wul.WeaponRarity == rarity)
+                                                        .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.ToString(), statusCode: 500);
+            }
+        }
+
+        // GET: api/WeaponLimits/level
+        [HttpGet("level")]
+        public async Task<ActionResult<IEnumerable<WeaponLevelLimit>>> GetWeaponLevelLimits(int? rarity)
+        {
+            try
+            {
+                return await _context.WeaponLevelLimits.Where(wll => rarity == null || wll.WeaponRarity == rarity)
+                                                       .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.ToString(), statusCode: 500);
+            }
+        }
+    }
+}
