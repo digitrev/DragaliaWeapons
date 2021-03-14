405959d86ed0942d2bd4d66f014f181d07305a6e
Trevor Burn <digitrev@gmail.com>
2021-03-13 23:44:56 -0500

    Add weapon limits
    Because figuring out fields and forms is hard

--- Dragalia/DragaliaApi/Controllers/WeaponLimitsController.cs
+++ Dragalia/DragaliaApi/Controllers/WeaponLimitsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DragaliaApi.Data;
 using DragaliaApi.Models;
+using DragaliaApi.Models.DTO;
 
 namespace DragaliaApi.Controllers
 {
@@ -19,6 +20,99 @@ namespace DragaliaApi.Controllers
         public WeaponLimitsController(DragaliaContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WeaponLimits>>> GetWeaponLimits()
+        {
+            try
+            {
+                return await _context.Weapons.Include(w => w.WeaponUpgrades)
+                    .ThenInclude(wu => wu.UpgradeType)
+                    .Join(_context.WeaponLevels.GroupBy(wl => wl.Rarity,
+                                                        (rarity, wl) => new { rarity, level = wl.Max(x => x.WeaponLevel1) }),
+                          w => w.Rarity,
+                          wl => wl.rarity,
+                          (w, wl) => new { w, wl.level }
+                          )
+                    .Select(x => new WeaponLimits
+                    {
+                        WeaponID = x.w.WeaponId,
+                        Level = x.level,
+                        Unbind = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Unbind")
+                                                   .Select(wu => wu.Step)
+                                                   .DefaultIfEmpty()
+                                                   .Max(),
+                        Refinement = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Refinement")
+                                                       .Select(wu => wu.Step)
+                                                       .DefaultIfEmpty()
+                                                       .Max(),
+                        Slots = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Slots")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max() +
+                                x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Dominion")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max(),
+                        Bonus = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Weapon Bonus")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max()
+                    })
+                    .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.ToString(), statusCode: 500);
+            }
+        }
+        
+        [HttpGet("{weaponID}")]
+        public async Task<ActionResult<WeaponLimits>> GetWeaponLimitsByWeapon(int weaponID)
+        {
+            try
+            {
+                return await _context.Weapons.Where(w => w.WeaponId == weaponID)
+                    .Include(w => w.WeaponUpgrades)
+                    .ThenInclude(wu => wu.UpgradeType)
+                    .Join(_context.WeaponLevels.GroupBy(wl => wl.Rarity,
+                                                        (rarity, wl) => new { rarity, level = wl.Max(x => x.WeaponLevel1) }),
+                          w => w.Rarity,
+                          wl => wl.rarity,
+                          (w, wl) => new { w, wl.level }
+                          )
+                    .Select(x => new WeaponLimits
+                    {
+                        WeaponID = x.w.WeaponId,
+                        Level = x.level,
+                        Unbind = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Unbind")
+                                                   .Select(wu => wu.Step)
+                                                   .DefaultIfEmpty()
+                                                   .Max(),
+                        Refinement = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Refinement")
+                                                       .Select(wu => wu.Step)
+                                                       .DefaultIfEmpty()
+                                                       .Max(),
+                        Slots = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Slots")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max() +
+                                x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Dominion")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max(),
+                        Bonus = x.w.WeaponUpgrades.Where(wu => wu.UpgradeType.UpgradeType1 == "Weapon Bonus")
+                                                  .Select(wu => wu.Step)
+                                                  .DefaultIfEmpty()
+                                                  .Max()
+                    })
+                    .FirstAsync();
+            }
+            catch (Exception ex)
+            {
+                return Problem(detail: ex.ToString(), statusCode: 500);
+            }
         }
 
         // GET: api/WeaponLimits/unbind
