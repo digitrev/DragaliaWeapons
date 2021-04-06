30dd8a8dffdce03320bf9abcb48714e8131f2a56
Trevor Burn <digitrev@gmail.com>
2021-04-05 21:06:52 -0400

    Reduce duplication, initialize data, load dragon "crafting"

--- Dragalia/DragaliaDB/den/Tables/DragonAdvancedCosts.sql
+++ Dragalia/DragaliaDB/den/Tables/DragonAdvancedCosts.sql
@@ -1,6 +1,4 @@
 ﻿CREATE TABLE [den].[DragonAdvancedCosts] (
-	[Dragon] NVARCHAR(50) NOT NULL
-	,[Greatsphere] INT NOT NULL
+	[Greatsphere] INT NOT NULL
 	,[Sphere] INT NOT NULL
-	,CONSTRAINT [PK_DragonAdvancedCosts] PRIMARY KEY ([Dragon])
 	)
--- Dragalia/DragaliaDB/den/Tables/DragonTrialCosts.sql
+++ Dragalia/DragaliaDB/den/Tables/DragonTrialCosts.sql
@@ -1,6 +1,4 @@
 ﻿CREATE TABLE [den].[DragonTrialCosts] (
-	[Dragon] NVARCHAR(50) NOT NULL
-	,[Sphere] INT NOT NULL
+	[Sphere] INT NOT NULL
 	,[Scale] INT NOT NULL
-	,CONSTRAINT [PK_DragonTrade] PRIMARY KEY ([Dragon])
 	)
