7f23ea1aeb461c7b1f5d0681714f055e817d4cb9
Amir Sufyani <amir.sufyani@gmail.com>
2021-04-19 22:39:35 -0400

    passive accordions

--- Dragalia/frontend/src/pages/Passives/AccountPassiveGroup.tsx
+++ Dragalia/frontend/src/pages/Passives/AccountPassiveGroup.tsx
@@ -4,13 +4,25 @@ import { FC } from 'react';
 import { AccountPassiveGroupData } from '../../api/DataInterfaces';
 import { gray5 } from '../../Styles';
 import { AccountPassive } from './AccountPassive';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { ElementIcon, ElementString } from '../../components/ElementIcon';
+import { WeaponIcon, WeaponString } from '../../components/WeaponIcon';
 
 interface Props {
   data: AccountPassiveGroupData;
+  accordionStatus?: { [key: number]: boolean };
+  updateAccordion?: (id: number, status: boolean) => void;
+  index: number;
 }
 
 export const AccountPassiveGroup: FC<Props> = ({
   data: { element, weaponType, passives },
+  updateAccordion,
+  accordionStatus,
+  index,
 }) => {
   return (
     <div
@@ -18,39 +30,60 @@ export const AccountPassiveGroup: FC<Props> = ({
         padding: 10px 0px;
       `}
     >
-      <div
-        css={css`
-          padding: 10px 0px;
-          font-size: 19px;
-        `}
+      <Accordion
+        expanded={(accordionStatus && accordionStatus[index]) || false}
+        onChange={(event, expanded) =>
+          updateAccordion && updateAccordion(index, expanded)
+        }
       >
-        {element} {weaponType}
-      </div>
-      <ul
-        css={css`
-          list-style: none;
-          margin: 10px 0 0 0;
-          padding: 0px 20px;
-          background-color: #fff;
-          border-bottom-left-radius: 4px;
-          border-bottom-right-radius: 4px;
-          box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
-        `}
-      >
-        {passives.map((passive) => (
-          <li
-            key={passive.passiveId}
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <div
             css={css`
-              border-top: 1px solid ${gray5};
-              ::first-of-type {
-                border-top: none;
-              }
+              padding: 10px 0px;
+              font-size: 19px;
             `}
           >
-            <AccountPassive data={passive} />
-          </li>
-        ))}
-      </ul>
+            {element} {weaponType}{' '}
+            {element !== 'None' && (
+              <ElementIcon element={element as ElementString} />
+            )}{' '}
+            {weaponType && (
+              <WeaponIcon weaponType={weaponType as WeaponString} />
+            )}
+          </div>
+        </AccordionSummary>
+        <AccordionDetails
+          css={css`
+            flex-direction: column;
+          `}
+        >
+          <ul
+            css={css`
+              list-style: none;
+              margin: 10px 0 0 0;
+              padding: 0px 20px;
+              background-color: #fff;
+              border-bottom-left-radius: 4px;
+              border-bottom-right-radius: 4px;
+              box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+            `}
+          >
+            {passives.map((passive) => (
+              <li
+                key={passive.passiveId}
+                css={css`
+                  border-top: 1px solid ${gray5};
+                  ::first-of-type {
+                    border-top: none;
+                  }
+                `}
+              >
+                <AccountPassive data={passive} />
+              </li>
+            ))}
+          </ul>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
