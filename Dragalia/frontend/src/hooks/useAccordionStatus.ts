8ec59d86d9a178f7bbacbfacdb6b367bc28a8304
Amir Sufyani <amir.sufyani@gmail.com>
2021-04-19 20:44:40 -0400

    add accordions

---
+++ Dragalia/frontend/src/hooks/useAccordionStatus.ts
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const useAccordionStatus = (expandedState: { [key: number]: boolean }) => {
+  const [accordionStatus, setAccordionStatus] = useState(
+    {} as { [key: number]: boolean },
+  );
+
+  const expandAll = () => {
+    setAccordionStatus((current) => {
+      return {
+        ...expandedState,
+      };
+    });
+  };
+  const collapseAll = () => {
+    setAccordionStatus({});
+  };
+
+  const updateAccordion = (id: number, status: boolean) => {
+    setAccordionStatus((current) => {
+      return {
+        ...current,
+        [id]: status,
+      };
+    });
+  };
+
+  return {
+    accordionStatus,
+    expandAll,
+    collapseAll,
+    updateAccordion,
+  };
+};
+
+export default useAccordionStatus;
