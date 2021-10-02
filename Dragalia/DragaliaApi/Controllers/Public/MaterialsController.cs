652ee44e7925e8b2a2247d3db174cc7b59d7635c
Trevor Burn <digitrev@gmail.com>
2021-10-02 19:43:50 -0400

    MaterialQuest cs to sql

--- Dragalia/DragaliaApi/Controllers/Public/MaterialsController.cs
+++ Dragalia/DragaliaApi/Controllers/Public/MaterialsController.cs
@@ -65,54 +65,5 @@ namespace DragaliaApi.Controllers.Public
             }
         }
 
-        [HttpGet("quests")]
-        public async Task<ActionResult<IEnumerable<MaterialQuestDTO>>> GetMaterialQuests(string materials)
-        {
-            string[] materialArray = materials != null ? materials.Split(',') : Array.Empty<string>();
-            try
-            {
-                var mqs = await _context.MaterialQuests.Where(mq => materialArray.Length == 0
-                                                                    || materialArray.Any(m => m == mq.MaterialId))
-                                                       .Include(mq => mq.Material)
-                                                       .ThenInclude(m => m.Category)
-                                                       .Include(mq => mq.Quest)
-                                                       .ToListAsync();
-
-                var allQuests = await _context.Quests.ToListAsync();
-
-                var newMqs = new List<MaterialQuest>();
-                var toRemove = new List<MaterialQuest>();
-
-                foreach (var mq in mqs)
-                {
-                    var toAdd = allQuests.Where(q => q.QuestId != mq.QuestId
-                                                     && q.SortPath.IsDescendantOf(mq.Quest.SortPath))
-                                         .Select(q => new MaterialQuest
-                                         {
-                                             Material = mq.Material,
-                                             MaterialId = mq.MaterialId,
-                                             Quest = q,
-                                             QuestId = q.QuestId
-                                         }).ToList();
-                    newMqs.AddRange(toAdd);
-
-                    if (toAdd.Any())
-                        toRemove.Add(mq);
-                }
-
-                mqs.AddRange(newMqs);
-                mqs = mqs.Except(toRemove).ToList();
-
-                return mqs.OrderBy(mq => mq.Quest.SortPath)
-                          .ThenBy(mq => mq.Material.SortPath)
-                          .Select(q => _mapper.Map<MaterialQuestDTO>(q))
-                          .ToList();
-
-            }
-            catch (Exception ex)
-            {
-                return Problem(detail: ex.ToString(), statusCode: 500);
-            }
-        }
     }
 }
