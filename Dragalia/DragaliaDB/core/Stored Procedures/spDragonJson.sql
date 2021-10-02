cedb523c18c16e956b9e93b91d852bcc29435cb4
Trevor Burn <digitrev@gmail.com>
2021-10-02 19:25:55 -0400

    Dragons from cs to sql

---
+++ Dragalia/DragaliaDB/core/Stored Procedures/spDragonJson.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spDragonJson]
+AS
+SELECT d.DragonID AS dragonId
+	,d.Dragon AS dragon
+	,d.Rarity AS rarity
+	,e.Element AS element
+FROM core.Dragon AS d
+INNER JOIN core.Element AS e ON e.ElementID = d.ElementID
+ORDER BY e.SortOrder
+	,d.Rarity DESC
+	,d.DragonID DESC
+FOR JSON PATH
