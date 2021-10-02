da5174c06961740626ed134cfdd1ca5bee1355c6
Trevor Burn <digitrev@gmail.com>
2021-10-02 19:24:11 -0400

    Adventurers

---
+++ Dragalia/DragaliaDB/core/Stored Procedures/spAdventurerJson.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spAdventurerJson]
+AS
+SELECT a.AdventurerID AS adventurerId
+	,a.Adventurer AS adventurer
+	,a.Rarity AS rarity
+	,e.Element AS element
+	,wt.WeaponType AS weaponType
+	,a.MCLimit AS mcLimit
+FROM core.Adventurer AS a
+INNER JOIN core.Element AS e ON e.ElementID = a.ElementID
+INNER JOIN core.WeaponType AS wt ON wt.WeaponTypeID = a.WeaponTypeID
+ORDER BY e.SortOrder
+	,a.WeaponTypeID
+	,a.Rarity DESC
+	,a.AdventurerID DESC
+FOR JSON PATH
