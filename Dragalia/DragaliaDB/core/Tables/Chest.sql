1a52e6141fa9b34016d2936904d80198e9b3b92d
Trevor Burn <digitrev@gmail.com>
2021-10-29 12:44:00 -0400

    Add unique key to chest, remove quantity from chest drop

--- Dragalia/DragaliaDB/core/Tables/Chest.sql
+++ Dragalia/DragaliaDB/core/Tables/Chest.sql
@@ -5,4 +5,8 @@
 	,CONSTRAINT [PK_Chest] PRIMARY KEY ([ChestID])
 	,CONSTRAINT [FK_Chest_ChestGroup] FOREIGN KEY ([ChestGroupID]) REFERENCES [core].[ChestGroup]([ChestGroupID])
 	,CONSTRAINT [FK_Chest_Quest] FOREIGN KEY ([QuestID]) REFERENCES [core].[Quest]([QuestID])
+	,CONSTRAINT [AK_Chest_ChestGroup_Quest] UNIQUE (
+		[ChestGroupID]
+		,[QuestID]
+		)
 	)
--- Dragalia/DragaliaDB/core/Tables/ChestDrop.sql
+++ Dragalia/DragaliaDB/core/Tables/ChestDrop.sql
@@ -2,7 +2,6 @@
 	[ChestDropID] INT NOT NULL IDENTITY
 	,[ChestID] INT NOT NULL
 	,[MaterialID] NVARCHAR(50) NOT NULL
-	,[Quantity] INT NOT NULL
 	,CONSTRAINT [PK_ChestDrop] PRIMARY KEY ([ChestDropID])
 	,CONSTRAINT [FK_ChestDrop_Chest] FOREIGN KEY ([ChestID]) REFERENCES [core].[Chest]([ChestID])
 	,CONSTRAINT [FK_ChestDrop_Material] FOREIGN KEY ([MaterialID]) REFERENCES [core].[Material]([MaterialID])
