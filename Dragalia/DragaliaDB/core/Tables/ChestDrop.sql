88f03026799e40dbd5a8f526ed8fd7ce8fad0e26
Trevor Burn <digitrev@gmail.com>
2021-10-29 12:17:57 -0400

    Material is nvarchar(50)

--- Dragalia/DragaliaDB/core/Tables/ChestDrop.sql
+++ Dragalia/DragaliaDB/core/Tables/ChestDrop.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [core].[ChestDrop] (
 	[ChestDropID] INT NOT NULL IDENTITY
 	,[ChestID] INT NOT NULL
-	,[MaterialID] INT NOT NULL
+	,[MaterialID] NVARCHAR(50) NOT NULL
 	,[Quantity] INT NOT NULL
 	,CONSTRAINT [PK_ChestDrop] PRIMARY KEY ([ChestDropID])
 	,CONSTRAINT [FK_ChestDrop_Chest] FOREIGN KEY ([ChestID]) REFERENCES [core].[Chest]([ChestID])
