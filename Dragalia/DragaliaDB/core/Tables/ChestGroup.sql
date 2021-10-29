dd7e188246e35a107eae5cf04a1b08923c56b280
Trevor Burn <digitrev@gmail.com>
2021-10-29 12:21:09 -0400

    Add quantity to chest group

--- Dragalia/DragaliaDB/core/Tables/ChestGroup.sql
+++ Dragalia/DragaliaDB/core/Tables/ChestGroup.sql
@@ -2,6 +2,7 @@
 	[ChestGroupID] INT NOT NULL IDENTITY
 	,[ChestGroup] NVARCHAR(50) NOT NULL
 	,[FrequencyID] INT NOT NULL
+	,[Quantity] INT NOT NULL
 	,CONSTRAINT [PK_ChestGroup] PRIMARY KEY ([ChestGroupID])
 	,CONSTRAINT [FK_ChestGroup_Frequency] FOREIGN KEY ([FrequencyID]) REFERENCES [core].[Frequency]([FrequencyID])
 	)
