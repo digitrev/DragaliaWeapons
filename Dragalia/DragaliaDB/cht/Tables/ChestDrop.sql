4e6e95fe802d69cf0538d115f809c6f77cc04504
Trevor Burn <digitrev@gmail.com>
2021-10-29 19:07:53 -0400

    Add drop weight to chestdrop

--- Dragalia/DragaliaDB/cht/Tables/ChestDrop.sql
+++ Dragalia/DragaliaDB/cht/Tables/ChestDrop.sql
@@ -2,6 +2,7 @@
 	[ChestDropID] INT NOT NULL IDENTITY
 	,[ChestID] INT NOT NULL
 	,[MaterialID] NVARCHAR(50) NOT NULL
+	,[DropWeight] INT NOT NULL CONSTRAINT [DF_ChestDrop_DropWeight] DEFAULT(1)
 	,CONSTRAINT [PK_ChestDrop] PRIMARY KEY ([ChestDropID])
 	,CONSTRAINT [FK_ChestDrop_Chest] FOREIGN KEY ([ChestID]) REFERENCES [cht].[Chest]([ChestID]) ON DELETE CASCADE
 	,CONSTRAINT [FK_ChestDrop_Material] FOREIGN KEY ([MaterialID]) REFERENCES [core].[Material]([MaterialID]) ON DELETE CASCADE
